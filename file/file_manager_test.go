4cb2e7f2e91f56a21bc8eb9c43c9e3ef45107e11
orange-kyoto <dev@rikiyaota.kyoto>
2024-12-16 14:09:01 +0900

    Implement basic file manager.

---
+++ file/file_manager_test.go
@@ -0,0 +1,139 @@
+package file
+
+import (
+	"bytes"
+	"os"
+	"path/filepath"
+	"testing"
+)
+
+const (
+	testDir   = "./testdb"
+	blockSize = 32
+)
+
+func cleanTestDir() {
+	os.RemoveAll(testDir)
+}
+
+func createTempFiles() {
+	os.Mkdir(testDir, 0755)
+	os.Create(filepath.Join(testDir, "tempfile1"))
+	os.Create(filepath.Join(testDir, "tempfile2"))
+}
+
+func TestDbDirectoryExists(t *testing.T) {
+	t.Skip("test1")
+	cleanTestDir()
+	defer cleanTestDir()
+
+	if _, err := os.Stat(testDir); os.IsExist(err) {
+		t.Fatalf("Expected %s to not exist, but it does", testDir)
+	}
+
+	fm := NewFileManager(testDir, blockSize)
+	defer fm.Close()
+
+	if _, err := os.Stat(testDir); os.IsNotExist(err) {
+		t.Fatalf("Expected %s to exist, but it does not", testDir)
+	}
+}
+
+func TestTempFilesCleaned(t *testing.T) {
+	t.Skip("test2")
+	cleanTestDir()
+	defer cleanTestDir()
+
+	createTempFiles()
+
+	matches, _ := filepath.Glob(filepath.Join(testDir, "temp*"))
+	if len(matches) != 2 {
+		t.Fatalf("Expected temp files to exist, but they do not, matches=%d", len(matches))
+	}
+
+	fm := NewFileManager(testDir, blockSize)
+	defer fm.Close()
+
+	matches, _ = filepath.Glob(filepath.Join(testDir, "temp*"))
+	if len(matches) != 0 {
+		t.Fatalf("Expected temp files to be deleted, but they were not, matches=%d", len(matches))
+	}
+}
+
+func TestReadWrite(t *testing.T) {
+	t.Skip("test3")
+	cleanTestDir()
+	defer cleanTestDir()
+
+	fm := NewFileManager(testDir, blockSize)
+	defer fm.Close()
+
+	// 整数値の読み書きの検証
+	block1 := NewBlockID("testfile", 0)
+	page1 := NewPage(blockSize)
+	page1.SetInt(4, 12345)
+	fm.Write(block1, page1)
+	page2 := NewPage(blockSize)
+	fm.Read(block1, page2)
+	value1 := page2.GetInt(4)
+	if value1 != 12345 {
+		t.Errorf("Expected %d, got %d", 12345, value1)
+	}
+
+	// バイト列の読み書きの検証
+	block2 := NewBlockID("testfile", 1)
+	page3 := NewPage(blockSize)
+	page3.SetBytes(0, []byte("test"))
+	fm.Write(block2, page3)
+	page4 := NewPage(blockSize)
+	fm.Read(block2, page4)
+	value2 := page4.GetBytes(0)
+	if !bytes.Equal(value2, []byte("test")) {
+		t.Errorf("Expected 'test', got '%s'", value2)
+	}
+	// もう一度ブロック0を読み込んで、壊れていないか確認
+	page5 := NewPage(blockSize)
+	fm.Read(block1, page5)
+	value3 := page5.GetInt(4)
+	if value3 != 12345 {
+		t.Errorf("Expected %d, got %d", 12345, value3)
+	}
+
+	// 文字列の読み書きの検証
+	block3 := NewBlockID("testfile", 2)
+	page6 := NewPage(blockSize)
+	page6.SetString(0, "hello world!")
+	fm.Write(block3, page6)
+	page7 := NewPage(blockSize)
+	fm.Read(block3, page7)
+	value4 := page7.GetString(0)
+	if value4 != "hello world!" {
+		t.Errorf("Expected 'hello world!', got '%s'", value4)
+	}
+}
+
+func TestAppend(t *testing.T) {
+	cleanTestDir()
+	defer cleanTestDir()
+
+	fm := NewFileManager(testDir, blockSize)
+	defer fm.Close()
+
+	// ブロック１つだけ書き込んでおく.
+	block1 := NewBlockID("testfile", 0)
+	page1 := NewPage(blockSize)
+	page1.SetString(4, "hello world!")
+	fm.Write(block1, page1)
+
+	// ファイルを拡張する
+	appendedBlockID := fm.Append("testfile")
+
+	// 新しく拡張されたブロックは空のバイト列が書き込まれている.
+	page2 := NewPage(blockSize)
+	fm.Read(appendedBlockID, page2)
+	value := page2.GetBytes(0)
+
+	if bytes.Equal(value, make([]byte, blockSize)) {
+		t.Errorf("Expected empty block, got %v", value)
+	}
+}
--- file/page.go
+++ file/page.go
@@ -12,36 +12,36 @@ const (
 )
 
 type Page struct {
-	data []byte
+	Data []byte
 }
 
 func NewPage(blocksize int) *Page {
-	return &Page{data: make([]byte, blocksize)}
+	return &Page{Data: make([]byte, blocksize)}
 }
 
 func NewPageFrom(b []byte) *Page {
-	return &Page{data: b}
+	return &Page{Data: b}
 }
 
 func (p *Page) GetInt(offset int) int32 {
-	return int32(binary.LittleEndian.Uint32(p.data[offset:]))
+	return int32(binary.LittleEndian.Uint32(p.Data[offset:]))
 }
 
 func (p *Page) GetBytes(offset int) []byte {
 	length := p.GetInt(offset)
 	blobOffset := offset + Int32ByteSize
 
-	return p.data[blobOffset : blobOffset+int(length)]
+	return p.Data[blobOffset : blobOffset+int(length)]
 }
 
 func (p *Page) SetInt(offset int, newData int32) {
-	binary.LittleEndian.PutUint32(p.data[offset:], uint32(newData))
+	binary.LittleEndian.PutUint32(p.Data[offset:], uint32(newData))
 }
 
 func (p *Page) SetBytes(offset int, newData []byte) {
 	p.SetInt(offset, int32(len(newData)))
 	blobOffset := offset + Int32ByteSize
-	copy(p.data[blobOffset:], newData)
+	copy(p.Data[blobOffset:], newData)
 }
 
 func (p *Page) GetString(offset int) string {
