4b36d294c80f917c5b31115179ed9c4b0f550f1a
orange-kyoto <dev@rikiyaota.kyoto>
2024-12-27 08:40:44 +0900

    Fix lock table tests.

--- transaction/lock_table_test.go
+++ transaction/lock_table_test.go
@@ -55,17 +55,20 @@ func TestLockWithoutConflicts(t *testing.T) {
 
 	t.Run("XLock x 1, Unlock x 1 が成功する.", func(t *testing.T) {
 		lockTable := NewLockTable()
-		testBlockId := file.NewBlockID("test_lock_table_1", 0)
-		lockTable.XLock(testBlockId)
+		testBlockID := file.NewBlockID("test_lock_table_1", 0)
 
-		assert.Len(t, lockTable.waitList, 0, "XLock は成功するので waitList は空であるべき.")
+		// 注意：ConcurrencyManager の実装で、XLock の前に SLock を獲得することが前提になっている.
+		lockTable.SLock(testBlockID)
+		lockTable.XLock(testBlockID)
 
-		lock, exists := lockTable.locks[testBlockId]
+		assert.Len(t, lockTable.waitList, 0, "SLock/XLock は成功するので waitList は空であるべき.")
+
+		lock, exists := lockTable.locks[testBlockID]
 		assert.True(t, exists, "XLock は成功するので locks に要素が存在するべき.")
 		assert.Equal(t, LockValue(-1), lock, "XLock は成功するので locks には LockValue(-1) が格納されているべき.")
 
-		lockTable.Unlock(testBlockId)
-		lock, exists = lockTable.locks[testBlockId]
+		lockTable.Unlock(testBlockID)
+		lock, exists = lockTable.locks[testBlockID]
 		assert.False(t, exists, "Unlock は成功するので locks から要素が削除されるべき.")
 		assert.Equal(t, LockValue(0), lock, "Unlock は成功するので locks から要素が削除されるべき.")
 	})
@@ -80,9 +83,13 @@ func TestLockWithSomeConflicts(t *testing.T) {
 		assert.Panics(t, func() { lockTable.SLock(testBlockID) }, "XLock が獲得されている時に SLock の獲得がブロックされるべき.")
 	})
 
+	t.Run("XLock が獲得されている時に XLock の獲得がブロックされる.", func(t *testing.T) {
+		t.Skip("ConcurrencyManager は XLock の前に SLock を獲得するという前提を置いているので、このテストは不要.")
+	})
+
 	t.Run("XLock が獲得されているためにブロックされていた SLock だが、XLock が解放されると直ちに SLock の取得ができる.", func(t *testing.T) {
 		lockTable := NewLockTable()
-		testBlockID := file.NewBlockID("test_lock_table_2", 0)
+		testBlockID := file.NewBlockID("test_lock_table_3", 0)
 		lockTable.XLock(testBlockID)
 
 		done1 := make(chan bool)
